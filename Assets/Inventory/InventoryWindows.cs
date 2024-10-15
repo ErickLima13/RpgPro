6154dd66d8b7dc9db5e0ceef88cf1d15810c7a7a
Erick Araujo <erickequipe7@gmail.com>
2024-10-15 11:28:23 -0300

    start inventory

---
+++ Assets/Inventory/InventoryWindows.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryWindows : MonoBehaviour
+{
+    public Sprite[] imageTab;
+
+    public Image[] tabs;
+
+    public GameObject[] contents;
+
+    public int idTabActive;
+
+
+    private void Start()
+    {
+        SelectTab(idTabActive);
+    }
+
+    public void SelectTab(int idTab)
+    {
+        foreach (Image i in tabs)
+        {
+            i.sprite = imageTab[0];
+        }
+
+        foreach (GameObject g in contents)
+        {
+            g.SetActive(false);
+        }
+
+        tabs[idTab].sprite = imageTab[1];
+        contents[idTab].SetActive(true);
+    }
+}
